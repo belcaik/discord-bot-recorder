baa5c9546a6d28091c6112cd4a011b5d8d710cc1
belcaik <belcaik@gmail.com>
2024-03-19 22:46:28 -0300

    refactor: :recycle: declare listening stream util as a class

--- src/lib/ListeningStream.ts
+++ src/lib/ListeningStream.ts
@@ -1,45 +1,72 @@
-import { createWriteStream } from "node:fs";
+import { createWriteStream, existsSync, mkdirSync } from "node:fs";
 import { pipeline } from "node:stream";
 import { EndBehaviorType, VoiceReceiver } from "@discordjs/voice";
 import type { User } from "discord.js";
 import * as prism from "prism-media";
 
-function getDisplayName(userId: string, user?: User) {
-    return user ? `${user.username}_${user.discriminator}` : userId;
-}
+class ListeningStream {
+    private static instance: ListeningStream;
 
-export function createListeningStream(
-    receiver: VoiceReceiver,
-    userId: string,
-    user?: User
-) {
-    const opusStream = receiver.subscribe(userId, {
-        end: {
-            behavior: EndBehaviorType.AfterSilence,
-            duration: 1000,
-        },
-    });
+    private constructor() {}
 
-    const oggStream = new prism.opus.OggDemuxer({
-
-    });
-
-    const filename = `./recordings/${Date.now()}-${getDisplayName(
-        userId,
-        user
-    )}.ogg`;
-
-    const out = createWriteStream(filename);
-
-    console.log(`👂 Started recording ${filename}`);
-
-    pipeline(opusStream, oggStream, out, (err) => {
-        if (err) {
-            console.warn(
-                `❌ Error recording file ${filename} - ${err.message}`
-            );
-        } else {
-            console.log(`✅ Recorded ${filename}`);
+    public static getInstance(): ListeningStream {
+        if (!ListeningStream.instance) {
+            ListeningStream.instance = new ListeningStream();
         }
-    });
+        return ListeningStream.instance;
+    }
+
+    public createListeningStream(
+        receiver: VoiceReceiver,
+        userId: string,
+        user?: User,
+        recordStartTimestamp?: number
+    ) {
+        const opusStream = receiver.subscribe(userId, {
+            end: {
+                behavior: EndBehaviorType.AfterSilence,
+                duration: 1000,
+            },
+        });
+
+        const oggStream = new prism.opus.OggLogicalBitstream({
+            opusHead: new prism.opus.OpusHead({
+                channelCount: 2,
+                sampleRate: 48000,
+            }),
+            pageSizeControl: {
+                maxPackets: 10,
+            }
+        });
+
+        const directoryName = `./recordings/${recordStartTimestamp}`;
+
+        // Create the directory if it does not exist
+        if (!existsSync(directoryName)) {
+            mkdirSync(directoryName, { recursive: true });
+        }
+
+        const filename = `${directoryName}/${Date.now()}-${this.getDisplayName(
+            userId,
+            user
+        )}.ogg`;
+
+        const out = createWriteStream(filename);
+
+        pipeline(opusStream, oggStream, out, (err) => {
+            if (err) {
+                console.warn(
+                    `❌ Error recording file ${filename} - ${err.message}`
+                );
+            } else {
+                console.log(`✅ Recorded ${filename}`);
+            }
+        });
+    }
+
+    private getDisplayName(userId: string, user?: User) {
+        return user ? `${user.username}_${user.discriminator}` : userId;
+    }
 }
+
+export default ListeningStream.getInstance();
